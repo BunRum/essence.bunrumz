0c7f9096d93297fe004deca409c86ce3b9d03110
BunRum <61174957+BunRum@users.noreply.github.com>
2022-02-21 22:40:23 -0800

    k

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Games from './Games';
-import LoadFlashGame from './Player';
 import Player from './Player';
 
 function App() {
+  console.log(window.location.pathname)
   return (
     <Router>
       <Routes>
-        <Route id="Games" exact path='/' exact element={<Games />} />
-        <Route id="Player" exact path='/Player' exact element={<Player />} />
+        <Route id="Games" path='/' exact element={<Games />} />
+        <Route id="Player" path='/Player' exact element={<Player/>} />
       </Routes>
     </Router>
   );
-}
-
-if (window.location.pathname = "/Player") {
-  LoadFlashGame()
 }
 export default App;
--- src/Player.js
+++ src/Player.js
@@ -1,31 +1,59 @@
 import React from "react";
-
-export default function LoadFlashGame(url) {
-    console.log("ran");
+import { useSearchParams } from "react-router-dom";
+let bun = 0
+export function LoadFlashGame(url) {
+    console.log("Ran")
+    console.log(bun)
+    bun = bun + 1
     let myScript = document.createElement("script");
     myScript.setAttribute("src", "https://bunrum.github.io/RuffleForGoogleSites/ruffle.js");
     document.body.appendChild(myScript);
-    myScript.addEventListener("load", scriptLoaded, false);
-    function scriptLoaded() {
-        window.RufflePlayer = window.RufflePlayer || {};
-        const ruffle = window.RufflePlayer.newest();
-        const player = ruffle.createPlayer();
-        const container = document.getElementById("container");
-        container.appendChild(player);
-        player.Config = {
-            "backgroundColor": "#000000",
-            "contextMenu": true,
-            "autoplay": "auto",
-            "letterbox": "on",
-            "unmuteOverlay": "visible",
-        };
-        player.load(url);
-        player.style.width = "100%";
-        player.style.height = "100%";
+
+    // if ((bun = 1)) {
+    //     console.log("log")
+    //     let myScript = document.createElement("script");
+    //     myScript.setAttribute("src", "https://bunrum.github.io/RuffleForGoogleSites/ruffle.js");
+    //     document.body.appendChild(myScript);
+    //     myScript.addEventListener("load", scriptLoaded, false);
+    //     function scriptLoaded() {
+    //         window.RufflePlayer = window.RufflePlayer || {};
+    //         const ruffle = window.RufflePlayer.newest();
+    //         const player = ruffle.createPlayer();
+    //         const container = document.getElementById("container");
+    //         console.log(container)
+    //         container.appendChild(player);
+    //         player.Config = {
+    //             "backgroundColor": "#000000",
+    //             "contextMenu": true,
+    //             "autoplay": "auto",
+    //             "letterbox": "on",
+    //             "unmuteOverlay": "visible",
+    //         };
+    //         player.style.width = "100%";
+    //         player.style.height = "100%";
+    //         player.load(url);
+    //         console.log("aa");
+    //     }
+    // }
+}
+
+const Games = {
+    "Riddle School": {
+        "type": "Flash",
+        "swf": "https://bunrum.github.io/RuffleForGoogleSites/riddleschool.swf"
+    }
+}
+
+function LoadGame(Game) {
+    if (Games[Game]["type"] = "Flash") {
+        console.log("is Flash")
+        LoadFlashGame(Games[Game]["swf"])
     }
 }
 
 export default function Player() {
+    let [searchParams, setSearchParams] = useSearchParams();
+    let game = searchParams.get("game")
     return (
         <div id="container" style={{
             position: 'absolute',
@@ -36,6 +64,7 @@ export default function Player() {
             width: '100vw',
             height: '100vh'
         }}>
+            {LoadGame(game)}
         </div>
     );
 };
